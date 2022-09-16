1a27abfd97023a5bd7d1995967687cc76fe3d3fb
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-09-16 02:25:30 +0200

    Add interactions on css

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const hamburger = document.querySelector('.bar');
+const navList = document.querySelector('.nav-list');
 
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navList.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navList.classList.remove('active');
+}));
